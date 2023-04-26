bdabbc1e0d81613c15a0ee4415afe81cd9713857
BenjMy <36472081+BenjMy@users.noreply.github.com>
2023-04-26 16:07:42 +0200

    pycathy instead of pyCATHY

--- doc/conf.py
+++ doc/conf.py
@@ -144,7 +144,7 @@ source_encoding = "utf-8"
 master_doc = "index"
 pygments_style = "default"
 add_function_parentheses = False
-jupyter_execute_notebooks_kernel = 'pyCATHY'
+jupyter_execute_notebooks_kernel = 'pycathy'
 
 
 # Sphinx-Gallery configuration
