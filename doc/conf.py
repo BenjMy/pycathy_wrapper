f1d78d096dd0defba13936788de7ac2b013f2a2b
BenjMy <36472081+BenjMy@users.noreply.github.com>
2023-05-09 18:45:48 +0200

    add google colab button

--- doc/conf.py
+++ doc/conf.py
@@ -225,6 +225,8 @@ html_theme_options = {
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
         "notebook_interface": "jupyterlab",
+        "colab_url": "https://colab.research.google.com",
+
     },
     "use_edit_page_button": True,
     "use_issues_button": True,
