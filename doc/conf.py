b2d2c52234f72b550741d551964988700a81e7fa
BenjMy <36472081+BenjMy@users.noreply.github.com>
2024-03-08 09:51:55 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -158,8 +158,8 @@ sphinx_gallery_conf = {
     # path where to save gallery generated examples
     #"gallery_dirs": "gallery",
     "gallery_dirs": ["content/SSHydro", "content/DA"],
-    "filename_pattern": r".ipynb",
-    #"filename_pattern": 'py'|'ipynb'},
+    #"filename_pattern": r".ipynb",
+    "filename_pattern": 'py'|'ipynb'},
     #"dont_preprocess": [],
     # Remove the "Download all examples" button from the top level gallery
     "download_all_examples": False,
